ed9a71c63d4f2a5c940889287258bed980083687
br-gaona <brandongaona03@hotmail.com>
2023-07-04 18:59:08 -0500

    Creating the index route

--- server/src/app.js
+++ server/src/app.js
@@ -23,6 +23,10 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use("/api", api);
 
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "success" });
+});
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
