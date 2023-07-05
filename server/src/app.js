582560d330b594bb4219d523d0f49a4adb7ebaf6
br-gaona <brandongaona03@hotmail.com>
2023-07-04 21:34:16 -0500

    Removing the http to https

--- server/src/app.js
+++ server/src/app.js
@@ -2,17 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const httpToHttps = require('express-http-to-https');
-
 const path = require("path");
 
 const api = require("./routes/api");
 const { getCurrent } = require("./models/data.model");
 
 const app = express();
-
-// Enable HTTP to HTTPS redirection
-app.use(httpToHttps.redirect());
 
 app.use(
   cors({
