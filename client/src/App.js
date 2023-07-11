b3729469a5ad0388b07f71aa1eccca82d4e5e098
br-gaona <brandongaona03@hotmail.com>
2023-07-11 11:30:40 -0500

    Providers changed

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,31 @@ import CardsPage from "./pages/CardsPage";
 import { UsersProvider } from "./context/usersContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ServersPage from "./pages/ServersPage";
+import UsersPage from "./pages/UsersPage";
+import { ServersProvider } from "./context/serversContext";
+import { DataProvider } from "./context/dataContext";
 
 function App() {
   return (
     <UsersProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/history" element={<TablePage />} />
-            <Route index path="/" element={<CardsPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-          </Route>
-          <Route path="*" element={<LoginPage />} />
-        </Routes>
-      </Router>
+      <DataProvider>
+        <ServersProvider>
+          <Router>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route element={<ProtectedRoute />}>
+                <Route path="/history" element={<TablePage />} />
+                <Route index path="/" element={<CardsPage />} />
+                <Route path="/servers" element={<ServersPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/users" element={<UsersPage />} />
+              </Route>
+              <Route path="*" element={<LoginPage />} />
+            </Routes>
+          </Router>
+        </ServersProvider>
+      </DataProvider>
     </UsersProvider>
   );
 }
